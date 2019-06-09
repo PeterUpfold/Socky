7a5226eb220a89542dcc73f601e1eb60775b8c71
PeterUpfold <pgithub@upfold.org.uk>
2019-06-09 03:51:23 +0100

    Initial commit

---
+++ Socky/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Socky
+//
+//  Created by Peter Upfold on 09/06/2019.
+//  Copyright © 2019 Peter Upfold. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
