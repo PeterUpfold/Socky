1f479ea9f7f05e77ae369c52f4f801f43f9c5d13
PeterUpfold <pgithub@upfold.org.uk>
2019-06-09 04:10:44 +0100

    Socket testing

---
+++ README.md
@@ -0,0 +1,10 @@
+Socky
+=====
+
+Listen on TCP sockets to observe connection attempts to servers on the local network.
+
+This is designed to provide some level of visibility into any activities on your system where local servers
+running on Well-Known (and merely well-known, if you catch my drift) ports are being enumerated.
+
+Inspired by the proof of concept of this demonstrated by https://wybiral.github.io/wtf/
+
--- Socky/main.c
+++ Socky/main.c
@@ -1,15 +1,44 @@
-//
-//  main.c
-//  Socky
-//
-//  Created by Peter Upfold on 09/06/2019.
-//  Copyright © 2019 Peter Upfold. All rights reserved.
-//
+/*
+ * Socky.
+ *
+ * Copyright (C) 2019 Peter Upfold.
+ *
+ * This file is licensed to you under the Apache License, version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
 
 #include <stdio.h>
+#import <CoreFoundation/CoreFoundation.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+void callback(CFSocketRef socket, CFSocketCallBackType type, CFDataRef address, const void *data, void *info) {
+    printf("Got a callback instance");
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    CFSocketRef socket;
+    
+    socket = CFSocketCreate(NULL, PF_INET, SOCK_STREAM, IPPROTO_TCP, kCFSocketReadCallBack, &callback, NULL);
+    
+    
     printf("Hello, World!\n");
+    
+    
+    
+    CFRunLoopRun();
+    CFRelease(socket);
+    
     return 0;
 }
+
